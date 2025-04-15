05104bcd486ed6ab98edacbb3203338b83b07626
jan-abelium <jan@LAPTOP-JAN>
2025-04-15 11:15:25 +0200

    fixed eip1193 test file

--- test/wallet/eip1193/index.ts
+++ test/wallet/eip1193/index.ts
@@ -1,13 +1,23 @@
 import { describe, it } from "node:test";
 import assert from "assert"
+import { existsSync, readFileSync } from "node:fs";
+import path from "node:path";
 import { EIP1193WalletController, EIP1193Wallet } from "../../../src"
 import { TestProvider } from "./provider"
-import accounts from "../../keys/eip1193_accounts.json"
 import { Transaction } from "ethers";
 
 async function execute() {
 
-    let provider = new TestProvider(accounts.map(a => a.private_key))
+    let accounts: any
+    const TEST_ACCOUNTS_FILE = path.join("test", "keys", "eip1193_accounts.json")
+    if (existsSync(TEST_ACCOUNTS_FILE)) {
+        accounts = JSON.parse(readFileSync(TEST_ACCOUNTS_FILE).toString())
+    } else {
+        console.info(`To execute tests provide json file ${TEST_ACCOUNTS_FILE} with a list of accounts, each containing 'private_key' and 'address' field.`)
+        return
+    }
+
+    let provider = new TestProvider(accounts.map((a: { private_key: string }) => a.private_key))
     let controller = new EIP1193WalletController(provider)
 
     describe("controller", () => {
